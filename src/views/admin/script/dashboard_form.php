eb5cba20fc4ffe7ed98722b55fc748573e530ad4
Technote <technote.space@gmail.com>
2019-04-11 16:00:49 +0900

    code formatting

--- src/views/admin/script/dashboard_form.php
+++ src/views/admin/script/dashboard_form.php
@@ -15,27 +15,29 @@ if ( ! defined( 'WP_CONTENT_FRAMEWORK' ) ) {
 /** @var \WP_Framework_Presenter\Interfaces\Presenter $instance */
 ?>
 <script>
-    (function ($) {
-        $(function () {
-            let submit = false;
-            $('form').submit(function () {
-                if (submit) return false;
-                submit = true;
-                $('form input, form select').prop('readonly', true).addClass('disabled').off('click').on('click', function () {
-                    return false;
-                });
-                return true;
-            });
+	( function ( $ ) {
+		$( function () {
+			let submit = false;
+			$( 'form' ).submit( function () {
+				if ( submit ) {
+					return false;
+				}
+				submit = true;
+				$( 'form input, form select' ).prop( 'readonly', true ).addClass( 'disabled' ).off( 'click' ).on( 'click', function () {
+					return false;
+				} );
+				return true;
+			} );
 
-            const $reset = $('.form-buttons input[name="reset"]');
-            if (!$._data($reset.get(0), 'events')) {
-                $reset.on('click', function () {
-                    if (window.confirm('<?php $instance->h( 'Are you sure to reset settings?', true );?>')) {
-                        $(this).closest('form').submit();
-                    }
-                    return false;
-                });
-            }
-        });
-    })(jQuery);
+			const $reset = $( '.form-buttons input[name="reset"]' );
+			if ( ! $._data( $reset.get( 0 ), 'events' ) ) {
+				$reset.on( 'click', function () {
+					if ( window.confirm( '<?php $instance->h( 'Are you sure to reset settings?', true );?>' ) ) {
+						$( this ).closest( 'form' ).submit();
+					}
+					return false;
+				} );
+			}
+		} );
+	} )( jQuery );
 </script>
